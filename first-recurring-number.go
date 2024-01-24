6eb5da33bb9ca51b9abebaae513525e32ace8272
Ricardo Nascimento <43101705+ricardonas@users.noreply.github.com>
2024-01-23 21:42:41 -0300

    Add find first recurring number.

---
+++ first-recurring-number.go
@@ -0,0 +1,12 @@
+func findFirstRecurringCharacter(input []int) int {
+  myMap := make(map[int]int)
+
+  for _, v := range input {
+    if _, ok := myMap[v]; ok {
+      return v
+    }
+    myMap[v]++
+  }
+
+  return 0
+}
